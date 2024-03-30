35659d56213351f296aa78054a650e941161e511
henryliu <17600047539@163.com>
2024-03-30 15:12:33 +0800

    update video in home page

--- apps/web/app/components/introduction.tsx
+++ apps/web/app/components/introduction.tsx
@@ -52,12 +52,12 @@ export const Introduction =()=>{
                     </p>
                 </div>
                 <div>
-                    <iframe width="570" height="320" src="https://www.youtube.com/embed/lOzCA7bZG_k" title="This is Nouns" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/CB56gMHqe3Y?si=NAcInSVmHTQ3XRK1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                 </div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-16">
                 <div>
-                    <iframe width="570" height="320" src="https://www.youtube.com/embed/oa79nN4gMPs" title="What is Nouns DAO and How Does it Work?" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/CB56gMHqe3Y?si=NAcInSVmHTQ3XRK1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                 </div>
                 <div>
                     <h2 className="text-4xl font-bold">Build With Objects.</h2>
